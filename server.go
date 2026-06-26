54d7cd18316721dd8d717266221b51cf88abfa1d
ICHINOSE Shogo <shogo82148@gmail.com>
2026-06-27 03:37:11 +0900

    add favicon

--- server.go
+++ server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	_ "embed" // for go:embed
 	"net/http"
 	"strconv"
 	"uuid"
 
 	"github.com/shogo82148/ridgenative"
 )
+
+//go:embed favicon.ico
+var favicon []byte
 
 type UUIDHandlerFunc func() uuid.UUID
 
@@ -17,10 +21,19 @@ func (g UUIDHandlerFunc) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	contentLength := len(b)
 	w.Header().Set("Content-Length", strconv.Itoa(contentLength))
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-store")
 	w.Write(b)
 }
 
+func faviconHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Length", strconv.Itoa(len(favicon)))
+	w.Header().Set("Content-Type", "image/x-icon")
+	w.Header().Set("Cache-Control", "public, max-age=604800") // 7 days
+	w.Write(favicon)
+}
+
 func main() {
+	http.HandleFunc("GET /favicon.ico", faviconHandler)
 	http.Handle("GET /", UUIDHandlerFunc(uuid.New))
 	http.Handle("GET /v4", UUIDHandlerFunc(uuid.NewV4))
 	http.Handle("GET /v7", UUIDHandlerFunc(uuid.NewV7))
